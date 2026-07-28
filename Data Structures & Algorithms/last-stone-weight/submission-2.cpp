ccd8d1a796286cc1b929938fde02f861cee136a4
Вабищевич Илья <116671906+vimori@users.noreply.github.com>
2026-07-28 18:42:58 +0300

    Add: last-stone-weight - submission-2

---
+++ Data Structures & Algorithms/last-stone-weight/submission-2.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        int maxStone = 0;
+        for (int stone : stones) {
+            maxStone = max(maxStone, stone);
+        }
+
+        vector<int> bucket(maxStone+1, 0);
+        for(int stone : stones){
+            bucket[stone]++;
+        }
+
+        int first = maxStone, second = maxStone;
+        while (first > 0) {
+            if(bucket[first] % 2 == 0){
+                first--;
+                continue;
+            }
+
+            int j = min(first - 1, second);
+            while(j > 0 && bucket[j] == 0) {
+                j--;
+            }
+
+            if (j == 0){
+                return first;
+            }
+
+            second = j;
+            bucket[first]--;
+            bucket[second]--;
+            bucket[first - second]++;
+            first = max(first - second, second);
+        }
+
+        return first; 
+    }
+};
